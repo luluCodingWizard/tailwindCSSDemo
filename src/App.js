c6e836040bd70877cb1b3a66eb35343185c52da7
lulucodingwizard <how.to.develop.fast@gmail.com>
2023-05-06 20:36:07 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const movies = [
+  {
+    id: 1,
+    title: "The Shawshank Redemption",
+    year: 1994,
+    image: "https://picsum.photos/id/1018/200/300",
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    year: 1972,
+    image: "https://picsum.photos/id/1023/200/300",
+  },
+  {
+    id: 3,
+    title: "The Dark Knight",
+    year: 2008,
+    image: "https://picsum.photos/id/1005/200/300",
+  },
+  {
+    id: 4,
+    title: "Schindler's List",
+    year: 1993,
+    image: "https://picsum.photos/id/1003/200/300",
+  },
+  {
+    id: 5,
+    title: "Forrest Gump",
+    year: 1994,
+    image: "https://picsum.photos/id/1002/200/300",
+  },
+];
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredMovies = movies.filter((movie) => {
+    return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <aside>
+        <h1>Top 5 Movies</h1>
+        <input
+          type="text"
+          placeholder="Search movies"
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </aside>
+      <ul>
+        {filteredMovies.map((movie) => (
+          <li key={movie.id}>
+            <img src={movie.image} alt={movie.title} />
+            <h2>{movie.title}</h2>
+            <p>Year: {movie.year}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
